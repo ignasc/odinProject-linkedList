73958b5470c0f77f2f4412f58eecc0fe73bdda0e
Ignas <15948796+ignasc@users.noreply.github.com>
2025-09-12 13:22:34 +0100

    Add templates for other functions that will be implemented.

--- classLinkedList.js
+++ classLinkedList.js
@@ -56,6 +56,34 @@ class LinkedList {
         };
         return currentNode.data;
     }
+
+    index(index){
+        console.log("index to be implemented")
+    }
+
+    pop(){
+        console.log("pop to be implemented")
+    }
+
+    contains(value){
+        console.log("contains to be implemented")
+    }
+
+    find(){
+        console.log("find to be implemented")
+    }
+
+    toString(){
+        console.log("toString to be implemented")
+    }
+
+    insertAt(index){
+        console.log("insertAt to be implemented")
+    }
+
+    remoteAt(index){
+        console.log("remoteAt to be implemented")
+    }
 };
 
 export default LinkedList;
